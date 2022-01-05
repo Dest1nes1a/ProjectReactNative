1eeaea7f4ab66cf14ab8da22bc04d1977b692f1d
Dest1nes1a <77435539+Dest1nes1a@users.noreply.github.com>
2022-01-05 11:07:03 +0700

    Ionicons-1/5/2021

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 
-const AboutScreen = () => {
+const AboutScreen = ({route}) => {
+    const {email} = route.params; //Navigation.params แต่มันนิยมใช้ สำหรับไปอีกหน้านึง
     return (
         <View style={styles.container}>
             <Text>AboutScreen</Text>
+            <Text>Email: {email}</Text>
         </View>
+        
     )
 }
 
